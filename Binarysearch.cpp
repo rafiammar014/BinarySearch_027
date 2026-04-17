0eabf83764f8c3a4550de3b6ce133cdf1c9cbd82
rafiammar014 <rafiammar014@gmail.com>
2026-04-17 14:43:46 +0700

    add input function to validate array length for binary search

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -4,3 +4,21 @@ using namespace std;
 int element[10];
 int Panjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan panjang (array maksimal 10): ";
+        cin >> Panjang;
+
+        if (Panjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
+}
